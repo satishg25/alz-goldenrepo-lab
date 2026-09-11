a7511dcfaaf8bf405ee85e87d1197df89fac5116
Satish <satish.gurushanthappa@gmail.com>
2026-09-11 13:27:22 +0100

    Bootstrap Terraform resource-group module and consumer example

---
+++ modules/resource-group/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  validation {
+    condition     = length(var.resource_group_name) > 0
+    error_message = "Resource Group Name cannot be empty."
+  }
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+  default     = "westeurope"
+}
