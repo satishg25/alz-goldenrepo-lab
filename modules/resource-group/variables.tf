0131291dcd6333f56fe8cc2714e4c04f6058ec5d
Satish <satish.gurushanthappa@gmail.com>
2026-09-14 11:20:28 +0100

    UC001 Create Azure Resource Group module

--- modules/resource-group/variables.tf
+++ modules/resource-group/variables.tf
@@ -2,7 +2,7 @@ variable "resource_group_name" {
   description = "Name of the Azure Resource Group"
   type        = string
   validation {
-    condition     = length(var.resource_group_name) > 0
+    condition     = length(trimspace(var.resource_group_name)) > 0
     error_message = "Resource Group Name cannot be empty."
   }
 }
@@ -11,4 +11,17 @@ variable "location" {
   description = "Azure Region"
   type        = string
   default     = "westeurope"
+
+  validation {
+    condition = contains(
+      [
+        "westeurope",
+        "northeurope",
+        "uksouth",
+        "ukwest"
+      ],
+      lower(var.location)
+    )
+    error_message = "Location must be a supported Azure region."
+  }
 }
